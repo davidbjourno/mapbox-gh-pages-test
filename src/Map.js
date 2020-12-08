76850659ee6c345671e33ec8fa1a10d67c29b95a
David Blood <9371095+davidbjourno@users.noreply.github.com>
2020-12-08 17:21:08 +0000

    Add map

---
+++ src/Map.js
@@ -0,0 +1,37 @@
+import React, { useEffect, useRef, useState } from "react";
+import mapboxgl from "mapbox-gl";
+import "./Map.css";
+
+mapboxgl.accessToken =
+  "pk.eyJ1IjoiYW1uZXN0eWRlY29kZXJzIiwiYSI6ImNraWc4MW5jczBjb2EyeG1xems1M3ExOHAifQ.eDn4KXZ5vR_sHKMzvjTapw";
+
+const Map = () => {
+  const [viewport, setViewport] = useState({
+    lng: 5,
+    lat: 34,
+    zoom: 2
+  });
+  const containerRef = useRef(null);
+
+  useEffect(() => {
+    const { lng, lat, zoom } = viewport;
+    const map = new mapboxgl.Map({
+      container: containerRef.current,
+      style: "mapbox://styles/mapbox/streets-v11",
+      center: [lng, lat],
+      zoom: zoom
+    });
+
+    map.on("move", () => {
+      setViewport({
+        lng: map.getCenter().lng.toFixed(4),
+        lat: map.getCenter().lat.toFixed(4),
+        zoom: map.getZoom().toFixed(2)
+      });
+    });
+  }, []);
+
+  return <div class="map__container" ref={containerRef} />;
+};
+
+export default Map;
